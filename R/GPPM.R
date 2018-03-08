21ddbb35e9831977af23fa00a794f6fe66e2b00c
Julian Karch <j.d.karch@fsw.leidenuniv.nl>
2018-03-08 18:21:45 +0100

    revert ducktape fix toSTan

--- R/GPPM.R
+++ R/GPPM.R
@@ -1,31 +1,3 @@
-validate_toStan <- function(parsedModel,myData){
-  stopifnot(is(parsedModel,'ParsedModel'))
-  stopifnot(is(myData,'StanData'))
-}
-
-toStan <-function(parsedModel,myData,control){
-  ##constants
-  templateLocation <- '~/mystuff/projects/GPPMSoftware/R/gppm/R/stanTemplate.stan'
-
-  validate_toStan(parsedModel,myData)
-
-
-  ##
-  theTemplate <- readChar(templateLocation, file.info(templateLocation)$size)
-
-
-  paramSect <- paste0('real ', parsedModel$params,';',collapse = '\n ')
-
-  theCode <- theTemplate;
-  theCode <- gsub('<parameters>',paramSect,theCode)
-  theCode <- gsub('<meanfunction>',parsedModel$mFormula,theCode)
-  theCode <- gsub('<covfunction>',parsedModel$kFormula,theCode)
-  if(control$stanModel){
-    theModel <- stan_model(model_code = theCode)
-  }
-}
-
-
 new_GPPM <- function(mFormula,cFormula,myData,control){
   stopifnot(is.character(mFormula))
   stopifnot(is.character(cFormula))
