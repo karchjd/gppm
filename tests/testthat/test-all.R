aff2134c85d90687bf226128459f59ca169df352
DESKTOP-303BJ9K\julian_work2 <j.d.karch@fsw.leidenuniv.nl>
2021-08-02 21:50:47 +0200

    removed all unneeded dependencies

--- tests/testthat/test-all.R
+++ tests/testthat/test-all.R
@@ -1,7 +1,6 @@
 context('all')
 test_that("all-1", {
   data("demoLGCM")
-  data('trueParas')
   lgcm <- gppm('muI+muS*t','varI+covIS*(t+t#)+varS*t*t#+(t==t#)*sigma',
                demoLGCM,'ID','x')
   expect(TRUE,TRUE)
