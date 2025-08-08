a945107cb6710d70d4e29dc37817d7b92a432c90
Julian Karch <karchjd@fsw.leideuniv.nl>
2025-08-08 17:01:05 +0200

    style improvements longData.R

--- R/LongData.R
+++ R/LongData.R
@@ -1,29 +1,36 @@
 new_LongData <- function(myData, ID, DV) {
-  stopifnot(is.character(ID) && length(ID) == 1)
-  stopifnot(is.character(DV) && length(DV) == 1)
-  stopifnot(is.data.frame(myData))
-
-  structure(myData, class = c("LongData", class(myData)), ID = ID, DV = DV)
+  stopifnot(
+    is.data.frame(myData),
+    is.character(ID), length(ID) == 1,
+    is.character(DV), length(DV) == 1
+  )
+  structure(myData,
+            class = c("LongData", class(myData)),
+            ID = ID,
+            DV = DV)
 }
 
 as_LongData <- function(myData, ID, DV) {
-  if (!"LongData" %in% class(myData)) {
-    myData <- new_LongData(myData, ID, DV)
-  } else if (!missing(ID)) {
+  if (!inherits(myData, "LongData")) {
+    return(new_LongData(myData, ID, DV))
+  }
+
+  if (!missing(ID)) {
     attr(myData, "ID") <- ID
-  } else if (!missing(DV)) {
+  }
+  if (!missing(DV)) {
     attr(myData, "DV") <- DV
   }
+
   myData
 }
 
-
 getID <- function(longData) {
-  stopifnot("LongData" %in% class(longData))
-  return(attr(longData, "ID"))
+  stopifnot(inherits(longData, "LongData"))
+  attr(longData, "ID")
 }
 
 getDV <- function(longData) {
-  stopifnot("LongData" %in% class(longData))
-  return(attr(longData, "DV"))
+  stopifnot(inherits(longData, "LongData"))
+  attr(longData, "DV")
 }
